b83e6ca0a80dcd9a068456aab62a94eed1ac7bf0
shuvosaif <shuvo.cse12@gmail.com>
2024-06-10 07:37:40 +0600

    Upload the last

--- src/components/Footer/FooterPart.jsx
+++ src/components/Footer/FooterPart.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const FooterPart = () => {
   return (
-    <div className='bg-black text-white'>
-      <div className='container mt-15 grid grid-cols-12'>
-        <div className='col-span-2'>
-          <h2 className='text-[24px]'>Exclusive</h2>
-          <h2 className='text-[18px]'>Subscribe</h2>
+    <div className="bg-black text-white">
+      <div className="container p-28 grid grid-cols-12">
+        <div className="col-span-3 flex flex-col gap-5">
+          <h2 className="text-[24px]">Exclusive</h2>
+          <h2 className="text-[18px]">Subscribe</h2>
           <p>Get 10% off your first order</p>
+          <button className="border-2 border-white h-10 w-45 rounded-lg ">
+            
+          </button>
         </div>
-        <div className='col-span-2'></div>
-        <div className='col-span-2'></div>
-        <div className='col-span-2'></div>
-        <div className='col-span-2'></div>
+        <div className="col-span-3 flex flex-col gap-5"></div>
+        <div className="col-span-2"></div>
+        <div className="col-span-2"></div>
+        <div className="col-span-2"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterPart
+export default FooterPart;
